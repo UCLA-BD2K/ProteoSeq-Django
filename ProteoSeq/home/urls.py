9d346b5ca440634c0b559219b297a12c0e806ffd
JasonYang96 <hsjfelix@gmail.com>
2016-07-21 11:53:44 -0700

    everything except the analysis pipeline is working

--- ProteoSeq/home/urls.py
+++ ProteoSeq/home/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
 from . import views
+
+app_name = 'home'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    
+    url(r'^contact-us',views.contact_us,name='contact-us'),
+    url(r'^doc',views.documentation,name='documentation')
 ]
--- ProteoSeq/home/views.py
+++ ProteoSeq/home/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'home/index.html')
+def contact_us(request):
+    return render(request,'home/contactus.html')
+def documentation(request):
+    return render(request,'home/doc.html')
